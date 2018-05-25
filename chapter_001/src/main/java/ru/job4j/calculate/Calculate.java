f34401884929c314da77721c08240f75827fcc08
Valerii Komar <val1988x@gmail.com>
2018-05-25 10:24:46 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Клас Calculate с методом main 
+ *
+ */
 public class Calculate{
-	
+
+	/**
+	 * Метод main с функицей вывода {@code "Hello World"} в консоли
+	 *
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
