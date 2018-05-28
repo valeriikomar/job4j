25f425edbf68b2c214290ed4118bf2b60309c377
Valerii Komar <val1988x@gmail.com>
2018-05-28 18:13:46 +0300

    2.4 Исспользование Git в IDEA[#31170]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,28 +1,24 @@
 package ru.job4j.calculate;
-
 /**
  * Calculate
- * 
  * @author Valerii Komar (val1988x@gmail.com)
  * @version $ld$
  * @since 0.1
  */
 public class Calculate {
-
-	/**
-	 * Main.
-	 * @param args - args.
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-	
-	/**
-	 * Method echo.
-	 * @param name Your name.
-	 * @return Echo plus your name.
-	 */
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
