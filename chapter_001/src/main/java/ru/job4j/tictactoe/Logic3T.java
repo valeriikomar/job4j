d8d9a1e4ca804a20f9687fa3e8c3fe321b09bfff
Valerii Komar <val1988x@gmail.com>
2018-07-08 16:47:30 +0300

    Крестики-нолики на JavaFX [#49777] (editing_1)

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -6,13 +6,108 @@ public class Logic3T {
     public Logic3T(Figure3T[][] table) {
         this.table = table;
     }
+    public boolean line(int i, int startX, int startY, int incX, int incY) {
+        boolean result = false;
+        int posX = incX;
+        int posY = incY;
+        switch (i) {
+            case 1:
+                boolean a = this.table[startX][startY].hasMarkX();
+                if (!a) {
+                    break;
+                }
+                for (int j = 1; j < this.table.length; j++) {
+                    if (a != this.table[startX + posX][startY + posY].hasMarkX()) {
+                        break;
+                    }
+                    if (j == this.table.length - 1) {
+                        result = true;
+                    }
+                    posX += incX;
+                    posY += incY;
+                }
+                break;
+
+            case 2:
+                boolean a1 = this.table[startX][startY].hasMarkO();
+                if (!a1) {
+                    break;
+                }
+                for (int j = 1; j < this.table.length; j++) {
+                    if (a1 != this.table[startX + posX][startY + posY].hasMarkO()) {
+                        break;
+                    }
+                    if (j == this.table.length - 1) {
+                        result = true;
+                    }
+                    posX += incX;
+                    posY += incY;
+                }
+                break;
+
+
+            default: result = false;
+        }
+        return result;
+    }
 
     public boolean isWinnerX() {
-        boolean a, b;
+        return line(1, 0, 0, 0, 1)
+               || line(1, 1, 0, 0, 1)
+               || line(1, 2, 0, 0, 1)
+               || line(1, 0, 0, 1, 0)
+               || line(1, 0, 1, 1, 0)
+               || line(1, 0, 2, 1, 0)
+               || line(1, 0, 0, 1, 1)
+               || line(1, 2, 0, -1, 1);
 
+/*      boolean result = false;
+        for (int i = 0; i < this.table.length - 1; i++) {
+            for (int j = 0; j < this.table.length - 1; j++) {
+                if (!this.table[i][j].hasMarkX()) {
+                    result = false;
+                    break;
+                }
+                if (j == this.table.length - 1) {
+                    result = true;
+                }
+            }
+            if (result) {
+                break;
+            }
+            for (int k = 0; k < this.table.length - 1; k++) {
+                if (!this.table[k][i].hasMarkX()) {
+                    result = false;
+                    break;
+                }
+                if (k == this.table.length - 1) {
+                    result = true;
+                }
+            }
+            for (int p = 0; p < this.table.length - 1; p++) {
+                if (!this.table[p][p].hasMarkX()) {
+                    result = false;
+                    break;
+                }
+                if (p == this.table.length - 1) {
+                    result = true;
+                }
+            }
+            for (int t = 0; t < this.table.length - 1; t++) {
+                if (!this.table[this.table.length - t][t].hasMarkX()) {
+                    result = false;
+                    break;
+                }
+                if (t == this.table.length - 1) {
+                    result = true;
+                }
+            }
+        }
+*/
+
+/*      boolean a, b;
         boolean a1 = this.table[0][0].hasMarkX();
         boolean b1 = this.table[this.table.length - 1][0].hasMarkX();
-
         for (int i = 0; i < this.table.length; i++) {
             a = this.table[i][0].hasMarkX();
             b = this.table[0][i].hasMarkX();
@@ -30,14 +125,21 @@ public class Logic3T {
             return true;
         }
         return false;
-    }
+*/  }
 
     public boolean isWinnerO() {
-        boolean a, b;
+        return line(2, 0, 0, 0, 1)
+                || line(2, 1, 0, 0, 1)
+                || line(2, 2, 0, 0, 1)
+                || line(2, 0, 0, 1, 0)
+                || line(2, 0, 1, 1, 0)
+                || line(2, 0, 2, 1, 0)
+                || line(2, 0, 0, 1, 1)
+                || line(2, 2, 0, -1, 1);
 
+/*      boolean a, b;
         boolean a1 = this.table[0][0].hasMarkO();
         boolean b1 = this.table[this.table.length - 1][0].hasMarkO();
-
         for (int i = 0; i < this.table.length; i++) {
             a = this.table[i][0].hasMarkO();
             b = this.table[0][i].hasMarkO();
@@ -54,7 +156,8 @@ public class Logic3T {
         if (a1 || b1) {
             return true;
         }
-        return false;
+*/
+
     }
 
     public boolean hasGap() {
